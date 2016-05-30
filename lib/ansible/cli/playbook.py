701ccc667f93d18a64b9ac6b886d51fe18b49e56
Adrian Likins <alikins@redhat.com>
2016-05-30 15:02:48 -0400

    wip experiment with pympler,objgraph mem profilers

--- lib/ansible/cli/playbook.py
+++ lib/ansible/cli/playbook.py
@@ -36,6 +36,28 @@ from ansible.utils.vars import load_extra_vars
 from ansible.utils.vars import load_options_vars
 from ansible.vars import VariableManager
 
+import inspect
+import objgraph
+from pympler import tracker
+from pympler.classtracker import ClassTracker
+
+import types
+from pympler.web import start_profiler
+
+ctracker = ClassTracker()
+ctracker.track_class(VariableManager)
+ctracker.track_class(Block)
+ctracker.track_class(PlayContext)
+ctracker.track_class(DataLoader)
+ctracker.track_class(Inventory)
+ctracker.track_class(PlaybookExecutor)
+ctracker.track_class(CLI)
+
+ctracker.create_snapshot()
+
+#memory_tracker = tracker.SummaryTracker()
+#memory_tracker.print_diff()
+
 try:
     from __main__ import display
 except ImportError:
@@ -86,10 +108,15 @@ class PlaybookCLI(CLI):
         display.verbosity = self.options.verbosity
         self.validate_conflicts(runas_opts=True, vault_opts=True, fork_opts=True)
 
+    def show_groth(self):
+        print()
+        objgraph.show_growth(limit=25)
+
     def run(self):
 
         super(PlaybookCLI, self).run()
-
+        ctracker.track_object(self)
+#        memory_tracker.print_diff()
         # Note: slightly wrong, this is written so that implicit localhost
         # Manage passwords
         sshpass    = None
@@ -97,6 +124,7 @@ class PlaybookCLI(CLI):
         vault_pass = None
         passwords = {}
 
+        self.show_groth()
         # don't deal with privilege escalation or passwords when we don't need to
         if not self.options.listhosts and not self.options.listtasks and not self.options.listtags and not self.options.syntax:
             self.normalize_become_options()
@@ -104,6 +132,10 @@ class PlaybookCLI(CLI):
             passwords = { 'conn_pass': sshpass, 'become_pass': becomepass }
 
         loader = DataLoader()
+        ctracker.track_object(loader)
+
+#        memory_tracker.print_diff()
+        self.show_groth()
 
         if self.options.vault_password_file:
             # read vault_pass from a file
@@ -123,14 +155,31 @@ class PlaybookCLI(CLI):
 
         # create the variable manager, which will be shared throughout
         # the code, ensuring a consistent view of global variables
+        print()
+        self.show_groth()
         variable_manager = VariableManager()
+        print()
+
+#        memory_tracker.print_diff()
+
+        self.show_groth()
         variable_manager.extra_vars = load_extra_vars(loader=loader, options=self.options)
-
+        print()
+        self.show_groth()
         variable_manager.options_vars = load_options_vars(self.options)
-
+        print()
+        self.show_groth()
         # create the inventory, and filter it based on the subset specified (if any)
         inventory = Inventory(loader=loader, variable_manager=variable_manager, host_list=self.options.inventory)
+        print()
+#
+#        memory_tracker.print_diff()
+        self.show_groth()
         variable_manager.set_inventory(inventory)
+        print()
+        self.show_groth()
+
+ #       memory_tracker.print_diff()
 
         # (which is not returned in list_hosts()) is taken into account for
         # warning if inventory is empty.  But it can't be taken into account for
@@ -149,9 +198,17 @@ class PlaybookCLI(CLI):
             raise AnsibleError("Specified --limit does not match any hosts")
 
         # create the playbook executor, which manages running the plays via a task queue manager
+        print()
+        self.show_groth()
+#        memory_tracker.print_diff()
         pbex = PlaybookExecutor(playbooks=self.args, inventory=inventory, variable_manager=variable_manager, loader=loader, options=self.options, passwords=passwords)
-
+        print()
+        self.show_groth()
+#        memory_tracker.print_diff()
         results = pbex.run()
+        print()
+        self.show_groth()
+#        memory_tracker.print_diff()
 
         if isinstance(results, list):
             for p in results:
@@ -200,6 +257,7 @@ class PlaybookCLI(CLI):
                         all_vars = variable_manager.get_vars(loader=loader, play=play)
                         play_context = PlayContext(play=play, options=self.options)
                         for block in play.compile():
+                            ctracker.track_object(block)
                             block = block.filter_tagged_tasks(play_context, all_vars)
                             if not block.has_tasks():
                                 continue
@@ -212,6 +270,67 @@ class PlaybookCLI(CLI):
 
                         display.display(taskmsg)
 
+            self.show_groth()
+            print()
+            objgraph.show_most_common_types(limit=50)
+            print()
+            objgraph.typestats()
+            print()
+            objgraph.show_refs(self, filename='sample-graph.png')
             return 0
         else:
+            #memory_tracker.print_diff()
+            print()
+            objgraph.show_most_common_types(limit=50)
+            print()
+            objgraph.typestats()
+            print()
+            #self.refs([loader, variable_manager, inventory, pbex])
+            #print(objgraph.get_leaking_objects([self]))
+            self.funs()
+
+            ctracker.create_snapshot()
+            #ctracker.stats.print_summary()
+
+            start_profiler(tracker=ctracker)
+            #pprint.pprint(sorted(objgraph.by_type('function')))
             return results
+
+    def refs(self, objs):
+
+        objgraph.show_refs(objs,
+                           filename='sample-graph-2.png',
+                           #filter=lambda x: not inspect.isclass(x),
+                           refcounts=True,
+                           shortnames=False,
+                           max_depth=5)
+        objgraph.show_backrefs(objs,
+                               refcounts=True,
+                               shortnames=False,
+                               filename='sample-graph-back-2.png',
+                               max_depth=5)
+
+    def funs(self):
+        objs = sorted(objgraph.by_type('function'))
+        import pprint
+
+        for obj in objs:
+            cls = ''
+            if '__module__' in dir(obj):
+                cls = cls + '%s' % obj.__module__
+            if '__class__' in dir(obj):
+                cls = cls + obj.__class__.__name__
+            print('id=%s name=%s module=%s cls=%s' % (id(obj), obj.__name__, obj.__module__, obj.__class__))
+            #print(id(obj))
+            #pprint.pprint(obj)
+
+    def leaks(self):
+        lks = objgraph.get_leaking_objects(objects=[self])
+        for objtype in lks:
+            print('type: %s by_type: %s' % (objtype, objgraph.by_type(objtype)))
+
+
+    def ltypes(self):
+        obs = objgraph.most_common_types(limit=100)
+        for objtype in obs:
+            print('type: %s by_type: %s' % (objtype, objgraph.by_type(objtype)))
