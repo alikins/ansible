1d58e6bdcb0fed07bc3aa5524823ae16f5320329
Adrian Likins <alikins@redhat.com>
2016-06-07 23:16:27 -0400

    unittest experiment

--- test/units/module_utils/test_basic.py
+++ test/units/module_utils/test_basic.py
@@ -26,17 +26,173 @@ import os
 import sys
 from io import BytesIO, StringIO
 
-try:
-    import builtins
-except ImportError:
-    import __builtin__ as builtins
+import logging
+log = logging.getLogger(__name__)
+
+from ansible.module_utils.six import PY2, PY3
+
+from nose.plugins.skip import SkipTest
 
 from units.mock.procenv import swap_stdin_and_argv
 
 from ansible.compat.tests import unittest
 from ansible.compat.tests.mock import patch, MagicMock, mock_open, Mock, call
 
-realimport = builtins.__import__
+
+realimport = __import__
+
+HAS_SELINUX = None
+try:
+    import selinux
+    HAS_SELINUX = True
+except ImportError:
+    pass
+
+class TestModuleUtilsBasicImporting(unittest.TestCase):
+
+    def setUp(self):
+        args = json.dumps(dict(ANSIBLE_MODULE_ARGS={}))
+        # unittest doesn't have a clean place to use a context manager, so we have to enter/exit manually
+        self.stdin_swap = swap_stdin_and_argv(stdin_data=args)
+        self.stdin_swap.__enter__()
+        self._imports_before = []
+        self._wrapped_imports = []
+        self.mod_to_fail = None
+
+    def tearDown(self):
+        # unittest doesn't have a clean place to use a context manager, so we have to enter/exit manually
+        self.stdin_swap.__exit__(None, None, None)
+        self.clear_modules(self._wrapped_imports)
+        self._wrapped_imports = []
+        self.mod_to_fail = None
+
+    def clear_modules(self, mods):
+        for mod in mods:
+            if mod in sys.modules:
+                log.debug('deleting module=%s', mod)
+                del sys.modules[mod]
+
+    def _import_wrapper(self, name, *args, **kwargs):
+        if name not in sys.modules:
+            self._wrapped_imports.append(name)
+        return realimport(name, *args, **kwargs)
+
+    def _mock_import(self, name, *args, **kwargs):
+        if self.mod_to_fail and (name == self.mod_to_fail):
+            raise ImportError
+        return self._import_wrapper(name, *args, **kwargs)
+
+    @patch('__builtin__.__import__')
+    def test_import_syslog_has_syslog(self, mock_import):
+        mock_import.side_effect = self._mock_import
+
+        import ansible.module_utils.basic as mod
+        self.assertTrue(mod.HAS_SYSLOG)
+
+    @patch('__builtin__.__import__')
+    def test_import_syslog_no_syslog(self, mock_import):
+        self.mod_to_fail = 'syslog'
+        mock_import.side_effect = self._mock_import
+
+        import ansible.module_utils.basic as mod
+        self.assertFalse(mod.HAS_SYSLOG)
+
+    @patch('__builtin__.__import__')
+    def test_import_selinux_has_selinux(self, mock_import):
+        if not HAS_SELINUX:
+            raise SkipTest('No selinux module to tests import of')
+
+        mock_import.side_effect = self._mock_import
+
+        import ansible.module_utils.basic as mod
+        self.assertTrue(mod.HAVE_SELINUX)
+
+    @patch('__builtin__.__import__')
+    def test_import_selinux_no_selinux(self, mock_import):
+        mock_import.side_effect = self._mock_import
+        self.mod_to_fail = "selinux"
+
+        import ansible.module_utils.basic as mod
+        self.assertFalse(mod.HAVE_SELINUX)
+
+    @patch('__builtin__.__import__')
+    def test_import_json_has_json(self, mock_import):
+        mock_import.side_effect = self._mock_import
+
+        import ansible.module_utils.basic as mod
+        self.assertTrue(hasattr(mod, 'json'))
+
+    @patch('__builtin__.__import__')
+    def test_import_json_no_json(self, mock_import):
+        mock_import.side_effect = self._mock_import
+        self.mod_to_fail = 'json'
+
+        try:
+            import ansible.module_utils.basic as mod
+        except SystemExit as e:
+            log.debug(e)
+
+    # FIXME: doesn't work yet
+    #@patch.object(builtins, 'bytes')
+    #def test_module_utils_basic_bytes(self, mock_bytes):
+    #    mock_bytes.side_effect = NameError()
+    #    from ansible.module_utils import basic
+
+    @patch('__builtin__.__import__')
+    @unittest.skipIf(sys.version_info[0] >= 3, "literal_eval is available in every version of Python3")
+    def test_import_literal_eval(self, mock_import):
+        def _mock_import(name, *args, **kwargs):
+            try:
+                fromlist = kwargs.get('fromlist', args[2])
+            except IndexError:
+                fromlist = []
+            if name == 'ast' and 'literal_eval' in fromlist:
+                raise ImportError
+            log.debug('name=%s', name)
+            return self._import_wrapper(name, *args, **kwargs)
+
+        mock_import.side_effect = _mock_import
+        import ansible.module_utils.basic as mod
+
+        # TODO: move tests of literal_eval to a different test class
+        self.assertEqual(mod.literal_eval("'1'"), "1")
+        self.assertEqual(mod.literal_eval("1"), 1)
+        self.assertEqual(mod.literal_eval("-1"), -1)
+        self.assertRaises(ValueError, mod.literal_eval, "asdfasdfasdf")
+        return
+
+        self.assertEqual(mod.module_utils.basic.literal_eval("(1,2,3)"), (1,2,3))
+        self.assertEqual(mod.module_utils.basic.literal_eval("[1]"), [1])
+        self.assertEqual(mod.module_utils.basic.literal_eval("True"), True)
+        self.assertEqual(mod.module_utils.basic.literal_eval("False"), False)
+        self.assertEqual(mod.module_utils.basic.literal_eval("None"), None)
+        # self.assertEqual(mod.module_utils.basic.literal_eval('{"a": 1}'), dict(a=1))
+
+    @patch('__builtin__.__import__')
+    def test_import_has_systemd_journal(self, mock_import):
+        def _mock_import(name, *args, **kwargs):
+            try:
+                fromlist = kwargs.get('fromlist', args[2])
+            except IndexError:
+                fromlist = []
+            if name == 'systemd' and 'journal' in fromlist:
+                raise ImportError
+            return realimport(name, *args, **kwargs)
+
+        mock_import.side_effect = _mock_import
+        import ansible.module_utils.basic as mod
+        log.debug('mod=%s', dir(mod))
+        self.assertTrue(mod.has_journal)
+
+    @patch('__builtin__.__import__')
+    def test_import_no_systemd_journal(self, mock_import):
+        mock_import.side_effect = self._mock_import
+        self.mod_to_fail = 'systemd'
+
+        import ansible.module_utils.basic as mod
+        log.debug('mod=%s', mod)
+        self.assertFalse(mod.has_journal)
+
 
 class TestModuleUtilsBasic(unittest.TestCase):
 
@@ -54,110 +210,6 @@ class TestModuleUtilsBasic(unittest.TestCase):
         for mod in mods:
             if mod in sys.modules:
                 del sys.modules[mod]
-
-    @patch.object(builtins, '__import__')
-    def test_module_utils_basic_import_syslog(self, mock_import):
-        def _mock_import(name, *args, **kwargs):
-            if name == 'syslog':
-                raise ImportError
-            return realimport(name, *args, **kwargs)
-
-        self.clear_modules(['syslog', 'ansible.module_utils.basic'])
-        mod = builtins.__import__('ansible.module_utils.basic')
-        self.assertTrue(mod.module_utils.basic.HAS_SYSLOG)
-
-        self.clear_modules(['syslog', 'ansible.module_utils.basic'])
-        mock_import.side_effect = _mock_import
-        mod = builtins.__import__('ansible.module_utils.basic')
-        self.assertFalse(mod.module_utils.basic.HAS_SYSLOG)
-
-    @patch.object(builtins, '__import__')
-    def test_module_utils_basic_import_selinux(self, mock_import):
-        def _mock_import(name, *args, **kwargs):
-            if name == 'selinux':
-                raise ImportError
-            return realimport(name, *args, **kwargs)
-
-        try:
-            self.clear_modules(['selinux', 'ansible.module_utils.basic'])
-            mod = builtins.__import__('ansible.module_utils.basic')
-            self.assertTrue(mod.module_utils.basic.HAVE_SELINUX)
-        except ImportError:
-            # no selinux on test system, so skip
-            pass
-
-        self.clear_modules(['selinux', 'ansible.module_utils.basic'])
-        mock_import.side_effect = _mock_import
-        mod = builtins.__import__('ansible.module_utils.basic')
-        self.assertFalse(mod.module_utils.basic.HAVE_SELINUX)
-
-    @patch.object(builtins, '__import__')
-    def test_module_utils_basic_import_json(self, mock_import):
-        def _mock_import(name, *args, **kwargs):
-            if name == 'json':
-                raise ImportError
-            elif name == 'simplejson':
-                return MagicMock()
-            return realimport(name, *args, **kwargs)
-
-        self.clear_modules(['json', 'ansible.module_utils.basic'])
-        mod = builtins.__import__('ansible.module_utils.basic')
-
-        self.clear_modules(['json', 'ansible.module_utils.basic'])
-        mock_import.side_effect = _mock_import
-        mod = builtins.__import__('ansible.module_utils.basic')
-
-    # FIXME: doesn't work yet
-    #@patch.object(builtins, 'bytes')
-    #def test_module_utils_basic_bytes(self, mock_bytes):
-    #    mock_bytes.side_effect = NameError()
-    #    from ansible.module_utils import basic
-
-    @patch.object(builtins, '__import__')
-    @unittest.skipIf(sys.version_info[0] >= 3, "literal_eval is available in every version of Python3")
-    def test_module_utils_basic_import_literal_eval(self, mock_import):
-        def _mock_import(name, *args, **kwargs):
-            try:
-                fromlist = kwargs.get('fromlist', args[2])
-            except IndexError:
-                fromlist = []
-            if name == 'ast' and 'literal_eval' in fromlist:
-                raise ImportError
-            return realimport(name, *args, **kwargs)
-
-        mock_import.side_effect = _mock_import
-        self.clear_modules(['ast', 'ansible.module_utils.basic'])
-        mod = builtins.__import__('ansible.module_utils.basic')
-        self.assertEqual(mod.module_utils.basic.literal_eval("'1'"), "1")
-        self.assertEqual(mod.module_utils.basic.literal_eval("1"), 1)
-        self.assertEqual(mod.module_utils.basic.literal_eval("-1"), -1)
-        self.assertEqual(mod.module_utils.basic.literal_eval("(1,2,3)"), (1,2,3))
-        self.assertEqual(mod.module_utils.basic.literal_eval("[1]"), [1])
-        self.assertEqual(mod.module_utils.basic.literal_eval("True"), True)
-        self.assertEqual(mod.module_utils.basic.literal_eval("False"), False)
-        self.assertEqual(mod.module_utils.basic.literal_eval("None"), None)
-        #self.assertEqual(mod.module_utils.basic.literal_eval('{"a": 1}'), dict(a=1))
-        self.assertRaises(ValueError, mod.module_utils.basic.literal_eval, "asdfasdfasdf")
-
-    @patch.object(builtins, '__import__')
-    def test_module_utils_basic_import_systemd_journal(self, mock_import):
-        def _mock_import(name, *args, **kwargs):
-            try:
-                fromlist = kwargs.get('fromlist', args[2])
-            except IndexError:
-                fromlist = []
-            if name == 'systemd' and 'journal' in fromlist:
-                raise ImportError
-            return realimport(name, *args, **kwargs)
-
-        self.clear_modules(['systemd', 'ansible.module_utils.basic'])
-        mod = builtins.__import__('ansible.module_utils.basic')
-        self.assertTrue(mod.module_utils.basic.has_journal)
-
-        self.clear_modules(['systemd', 'ansible.module_utils.basic'])
-        mock_import.side_effect = _mock_import
-        mod = builtins.__import__('ansible.module_utils.basic')
-        self.assertFalse(mod.module_utils.basic.has_journal)
 
     def test_module_utils_basic_get_platform(self):
         with patch('platform.system', return_value='foo'):
