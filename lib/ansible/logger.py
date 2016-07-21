c9ff22b07be2086bb2ea4f86af24a28b2ee3636c
Adrian Likins <alikins@redhat.com>
2016-07-21 17:41:47 -0400

    start replacing display with logging

---
+++ lib/ansible/logger.py
@@ -0,0 +1,34 @@
+# logging support for ansible using stdlib logging
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+
+# TODO: config based logging setup
+# TODO: Any log Formatters or Handlers that are ansible specific
+# TODO: base logging setup
+# TODO: NullHandler for py2.4
+
+# logging.INFO = 20
+V = 17
+VV = 16
+VVV = 15
+# logging.DEBUG = 10
+VVVV = 9
+VVVVV = 10
+
+logging.basicConfig(level=logging.DEBUG, filename='/home/adrian/ansible.log')
+#logging.basicConfig(level=logging.DEBUG,)
