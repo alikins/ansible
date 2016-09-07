ccf7b7491464f817bc1001d3e7b2f6c7dc4d8cfe
Adrian Likins <alikins@redhat.com>
2016-09-07 18:27:09 -0400

    add example log format for existing display format

--- lib/ansible/logger.py
+++ lib/ansible/logger.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
+import getpass
 import logging
 
 # TODO: config based logging setup
@@ -29,6 +30,23 @@ VVV = 15
 # logging.DEBUG = 10
 VVVV = 9
 VVVVV = 10
-DEBUG_LOG_FORMAT = "%(asctime)s [%(name)s %(levelname)s %(playbook)s] (%(process)d):%(funcName)s:%(lineno)d - %(message)s"
-logging.basicConfig(level=logging.DEBUG, filename='/home/adrian/ansible.log')
+#DEBUG_LOG_FORMAT = "%(asctime)s [%(name)s %(levelname)s %(playbook)s] (%(process)d):%(funcName)s:%(lineno)d - %(message)s"
+DEBUG_LOG_FORMAT = "%(asctime)s [%(name)s %(levelname)s] (%(process)d):%(funcName)s:%(lineno)d - %(message)s"
+
+# rough approx of existing display format
+# based on:
+#logger = logging.getLogger("p=%s u=%s | " % (mypid, user))
+# logging.basicConfig(filename=path, level=logging.DEBUG, format='%(asctime)s %(name)s %(message)s')
+# self.display("<%s> %s" % (host, msg), color=C.COLOR_VERBOSE, screen_only=True)
+# user and hostname attributes would be up to a logging.Filter to add
+# DISPLAY_LOG_FORMAT = "%(asctime)s p=%(process)d u=%(user)s <%(hostname)s> %(message)s"
+
+user = getpass.getuser()
+hostname = 'FIXME'
+DISPLAY_LOG_FORMAT = "%(asctime)s p=%(process)d u=" + user + " <" + hostname + "> " + "%(message)s"
+
+logging.basicConfig(level=logging.DEBUG,
+                    filename='/home/adrian/ansible.log',
+                    #format=DEBUG_LOG_FORMAT)
+                    format=DISPLAY_LOG_FORMAT)
 #logging.basicConfig(level=logging.DEBUG,)
