cedd8d7465d2e0ea270020b6d5d6e7226455a1f1
Adrian Likins <alikins@redhat.com>
2016-09-07 17:39:14 -0400

    merge cleanups

--- lib/ansible/plugins/callback/__init__.py
+++ lib/ansible/plugins/callback/__init__.py
@@ -29,7 +29,6 @@ from ansible import constants as C
 from ansible.module_utils._text import to_text
 from ansible.utils.color import stringc
 from ansible.vars import strip_internal_keys
-from ansible.utils.unicode import to_unicod
 from ansible import logger
 
 
