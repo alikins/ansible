0514281733843718f98adb44a266b818bd6df315
Adrian Likins <alikins@redhat.com>
2016-09-20 10:06:27 -0400

    use logging_tree if available

--- lib/ansible/cli/__init__.py
+++ lib/ansible/cli/__init__.py
@@ -51,6 +51,13 @@ except ImportError:
     from ansible.utils.display import Display
     display = Display()
 
+LOGGING_TREE = False
+try:
+    import logging_tree
+    LOGGING_TREE = True
+except ImportError:
+    pass
+
 
 class SortedOptParser(optparse.OptionParser):
     '''Optparser which sorts the options by opt before outputting --help'''
@@ -167,6 +174,9 @@ class CLI(with_metaclass(ABCMeta, object)):
             display.v(u"Using %s as config file" % to_text(C.CONFIG_FILE))
         else:
             display.v(u"No config file found; using defaults")
+
+        if LOGGING_TREE:
+            logging_tree.printout()
 
     @staticmethod
     def split_vault_id(vault_id):
