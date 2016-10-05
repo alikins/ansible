982ce2818125095b224f51ed2745bdfeb82669ff
Adrian Likins <alikins@redhat.com>
2016-10-05 13:21:41 -0400

    Add levels module with log level data

---
+++ lib/ansible/logger/levels.py
@@ -0,0 +1,15 @@
+
+class Levels(object):
+    # logging.INFO = 20
+    V = 17
+    VV = 16
+    VVV = 15
+    # logging.DEBUG = 10
+    VVVV = 9
+    VVVVV = 10
+
+V = Levels.V
+VV = Levels.VV
+VVV = Levels.VVV
+VVVV = Levels.VVVV
+VVVVV = Levels.VVVVV
