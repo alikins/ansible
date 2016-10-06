e02dc51f25b2e8b38986fcb6e112e14b52c9cd86
Adrian Likins <alikins@redhat.com>
2016-10-06 14:06:40 -0400

    populate 'remote_addr' for host loggers

--- lib/ansible/plugins/connection/__init__.py
+++ lib/ansible/plugins/connection/__init__.py
@@ -91,7 +91,8 @@ class ConnectionBase(with_metaclass(ABCMeta, object)):
         # Will make for a slightly odd logger name.
         # TODO/FIXME: do we want a top level logging namespace for this?
         # TODO/FIXME: recreate if hostname changes?
-        self.host_log = HostLoggerAdapter(log, extra={'remote_addr': self._play_context.remote_addr})
+        self.host_log = HostLoggerAdapter(log, extra={'remote_addr': self._play_context.remote_addr,
+                                                      'remote_user': self._play_context.remote_user})
         self.host_log.debug('init of self.host_log done')
 
         # load the shell plugin for this action/connection
