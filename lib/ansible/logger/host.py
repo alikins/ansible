8a6cfe2e42561906e0c15de24de882730e705166
Adrian Likins <alikins@redhat.com>
2016-10-06 13:41:49 -0400

    Make host.HostLoggerAdapter a name only class for now
    Rely on the default process() impl, since we don't need
    to change the message directly now.

--- lib/ansible/logger/host.py
+++ lib/ansible/logger/host.py
@@ -2,6 +2,9 @@
 import logging
 
 
+# This uses the default behavor of LoggerAdapter, adding the
+# dict like 'extra' object passed to it's init to the log records __dict__
+# so the keys in 'extra' can be used in custom log formats. Where we
+# use this class, we include 'remote_addr' in the dict
 class HostLoggerAdapter(logging.LoggerAdapter):
-    def process(self, msg, kwargs):
-        return '<%s> %s' % (self.extra['remote_addr'], msg), kwargs
+    pass
