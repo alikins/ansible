18397c1acaef4d1339fe889f19d4d7a329aff83d
Adrian Likins <alikins@redhat.com>
2016-10-24 11:15:24 -0400

    fix missing 'logger' import

--- lib/ansible/cli/pull.py
+++ lib/ansible/cli/pull.py
@@ -34,6 +34,7 @@ from ansible.cli import CLI
 from ansible.module_utils._text import to_native
 from ansible.plugins import module_loader
 from ansible.utils.cmd_functions import run_cmd
+from ansible import logger
 
 try:
     from __main__ import display
