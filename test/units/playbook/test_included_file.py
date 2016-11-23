e073cdd5c84ff6186c1c0c1acb1849f801e3bb13
Adrian Likins <alikins@redhat.com>
2016-11-23 11:01:10 -0500

    add unit tests for playbook/included_file.py

---
+++ test/units/playbook/test_included_file.py
@@ -0,0 +1,87 @@
+# (c) 2016, Adrian Likins <alikins@redhat.com>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# Make coding more python3-ish
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.compat.tests import unittest
+from ansible.compat.tests.mock import MagicMock
+
+from ansible.compat.six import string_types
+from ansible.errors import AnsibleParserError
+from ansible.playbook.attribute import FieldAttribute
+from ansible.playbook.task import Task
+from ansible.playbook.task_include import TaskInclude
+from ansible.template import Templar
+from ansible.executor import task_result
+
+from ansible.playbook import included_file
+
+
+from units.mock.loader import DictDataLoader
+
+
+class TestIncludedFile(unittest.TestCase):
+    def test(self):
+        inc_file = included_file.IncludedFile(filename='somefile.yml', args=[], task=None)
+        print(inc_file)
+
+    def test_process_include_results_empty_results(self):
+
+        hostname = "testhost1"
+        task_ds = {'action': 'include',
+                   'include_file': 'include_test.yml'}
+        task = TaskInclude()
+        loaded_task = task.load(task_ds)
+        print('loaded_task=%s' % loaded_task)
+        return_data = {'include': 'include_test.yml'}
+        result = task_result.TaskResult(host=hostname, task=loaded_task, return_data=return_data)
+        results = [result]
+        fake_loader = DictDataLoader({'include_test.yml': ""})
+        mock_tqm = MagicMock(name='MockTaskQueueManager')
+
+        mock_play = MagicMock(name='MockPlay')
+
+        mock_iterator = MagicMock(name='MockIterator')
+        mock_iterator._play = mock_play
+        #mock_iterator.mark_host_failed.return_value = None
+        #mock_iterator.get_next_task_for_host.return_value = (None, None)
+        #mock_iterator.get_original_task.return_value = mock_task
+
+        mock_inventory = MagicMock(name='MockInventory')
+        mock_inventory._hosts_cache = dict()
+
+        def _get_host(host_name):
+            print('_get_host hostname=%s' % host_name)
+            return None
+
+        mock_inventory.get_host.side_effect = _get_host
+        #mock_inventory.get_group.side_effect = _get_group
+        #mock_inventory.clear_pattern_cache.return_value = None
+        #mock_inventory.clear_group_dict_cache.return_value = None
+        #mock_inventory.get_host_vars.return_value = {}
+
+        # TODO: can we use a real VariableManager?
+        mock_variable_manager = MagicMock(name='MockVariableManager')
+        mock_variable_manager.get_vars.return_value = dict()
+
+        res = included_file.IncludedFile.process_include_results(results, mock_tqm, mock_iterator,
+                                                                 mock_inventory, fake_loader,
+                                                                 mock_variable_manager)
+
+        print(res)
