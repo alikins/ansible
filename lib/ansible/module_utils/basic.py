2eefb74f762d3cec47d01d6b806965444fb399a8
Adrian Likins <alikins@redhat.com>
2016-12-21 14:06:07 -0500

    fixup kwargs passing

--- lib/ansible/module_utils/basic.py
+++ lib/ansible/module_utils/basic.py
@@ -2056,7 +2056,7 @@ class AnsibleModule(object):
         return kwargs
 
     def exit_json(self, **kwargs):
-        kwargs = self._format_exit_json(kwargs)
+        kwargs = self._format_exit_json(**kwargs)
         raise AnsibleModuleExit(return_code=0,
                                 exception_data=kwargs)
 
@@ -2072,7 +2072,7 @@ class AnsibleModule(object):
         return kwargs
 
     def fail_json(self, **kwargs):
-        kwargs = self._format_fail_json(kwargs)
+        kwargs = self._format_fail_json(**kwargs)
         raise AnsibleModuleFatalError(return_code=1,
                                       exception_data=kwargs)
 
