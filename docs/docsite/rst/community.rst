6f5188df20d82f7aba6ca94d834728aaf0e47863
Adrian Likins <alikins@redhat.com>
2017-01-17 14:24:30 -0500

    split of modules into their own docs

--- docs/docsite/rst/community.rst
+++ docs/docsite/rst/community.rst
@@ -1,3 +1,5 @@
+.. _community-label:
+
 Community Information & Contributing
 ````````````````````````````````````
 
--- docs/docsite/rst/conf.py
+++ docs/docsite/rst/conf.py
@@ -38,7 +38,8 @@ AUTHOR='Ansible, Inc'
 # Add any Sphinx extension module names here, as strings.
 # They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.intersphinx']
 
 # Later on, add 'sphinx.ext.viewcode' to the list if you want to have
 # colorized code generated too for references.
