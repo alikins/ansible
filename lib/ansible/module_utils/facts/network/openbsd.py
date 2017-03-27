e2cebe722538ec441f921532803bf2464ab97121
Adrian Likins <alikins@redhat.com>
2017-03-27 15:46:46 -0400

    mv open/net bsd network fact class -> facts/network/*.py

---
+++ lib/ansible/module_utils/facts/network/openbsd.py
@@ -0,0 +1,18 @@
+from ansible.module_utils.facts import GenericBsdIfconfigNetwork
+
+
+class OpenBSDNetwork(GenericBsdIfconfigNetwork):
+    """
+    This is the OpenBSD Network Class.
+    It uses the GenericBsdIfconfigNetwork.
+    """
+    platform = 'OpenBSD'
+
+    # OpenBSD 'ifconfig -a' does not have information about aliases
+    def get_interfaces_info(self, ifconfig_path, ifconfig_options='-aA'):
+        return super(OpenBSDNetwork, self).get_interfaces_info(ifconfig_path, ifconfig_options)
+
+    # Return macaddress instead of lladdr
+    def parse_lladdr_line(self, words, current_if, ips):
+        current_if['macaddress'] = words[1]
+        current_if['type'] = 'ether'
