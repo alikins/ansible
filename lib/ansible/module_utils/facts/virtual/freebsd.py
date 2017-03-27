4d300ee21f8c6dbc4c3942835092c2b5d1eeb0d1
Adrian Likins <alikins@redhat.com>
2017-03-27 16:18:24 -0400

    FreeBSDVirtual -> facts/virtual/freebsd.py

---
+++ lib/ansible/module_utils/facts/virtual/freebsd.py
@@ -0,0 +1,22 @@
+import os
+
+from ansible.module_utils.facts import Virtual
+
+
+class FreeBSDVirtual(Virtual):
+    """
+    This is a FreeBSD-specific subclass of Virtual.  It defines
+    - virtualization_type
+    - virtualization_role
+    """
+    platform = 'FreeBSD'
+
+    def get_virtual_facts(self):
+
+        # Set empty values as default
+        self.facts['virtualization_type'] = ''
+        self.facts['virtualization_role'] = ''
+
+        if os.path.exists('/dev/xen/xenstore'):
+            self.facts['virtualization_type'] = 'xen'
+            self.facts['virtualization_role'] = 'guest'
