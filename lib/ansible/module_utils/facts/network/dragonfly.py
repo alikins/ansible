fd5d6ebef5e5dbe64952bb702825b5bb15ebabe6
Adrian Likins <alikins@redhat.com>
2017-03-27 15:39:18 -0400

    mv DragonFlyNetwork to facts/network/dragonfly.py

---
+++ lib/ansible/module_utils/facts/network/dragonfly.py
@@ -0,0 +1,9 @@
+from ansible.module_utils.facts import GenericBsdIfconfigNetwork
+
+
+class DragonFlyNetwork(GenericBsdIfconfigNetwork):
+    """
+    This is the DragonFly Network Class.
+    It uses the GenericBsdIfconfigNetwork unchanged.
+    """
+    platform = 'DragonFly'
