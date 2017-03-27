092fe9caf5100f7a1e913d34aece0b74023dfa99
Adrian Likins <alikins@redhat.com>
2017-03-27 16:36:23 -0400

    open/net bsd virtual fact class -> fact/virtual/*bsd.py

--- lib/ansible/module_utils/facts/virtual/__init__.py
+++ lib/ansible/module_utils/facts/virtual/__init__.py
@@ -1,13 +1,11 @@
 
-#from ansible.module_utils.facts.virtual import aix
-#from ansible.module_utils.facts.virtual import darwin
 from ansible.module_utils.facts.virtual import dragonfly
 from ansible.module_utils.facts.virtual import freebsd
 from ansible.module_utils.facts.virtual import hpux
-#from ansible.module_utils.facts.virtual import hurd
 from ansible.module_utils.facts.virtual import linux
-#from ansible.module_utils.facts.virtual import netbsd
-#from ansible.module_utils.facts.virtual import openbsd
+from ansible.module_utils.facts.virtual import netbsd
+from ansible.module_utils.facts.virtual import openbsd
 from ansible.module_utils.facts.virtual import sunos
+from ansible.module_utils.facts.virtual import sysctl
 
-__all__ = [dragonfly, freebsd, hpux, linux, sunos]
+__all__ = [dragonfly, freebsd, hpux, linux, netbsd, openbsd, sunos, sysctl]
---
+++ lib/ansible/module_utils/facts/virtual/openbsd.py
@@ -0,0 +1,35 @@
+import re
+
+from ansible.module_utils.facts import Virtual
+from ansible.module_utils.facts.virtual.sysctl import VirtualSysctlDetectionMixin
+
+from ansible.module_utils.facts.utils import get_file_content
+
+
+class OpenBSDVirtual(Virtual, VirtualSysctlDetectionMixin):
+    """
+    This is a OpenBSD-specific subclass of Virtual.  It defines
+    - virtualization_type
+    - virtualization_role
+    """
+    platform = 'OpenBSD'
+    DMESG_BOOT = '/var/run/dmesg.boot'
+
+    def get_virtual_facts(self):
+
+        # Set empty values as default
+        self.facts['virtualization_type'] = ''
+        self.facts['virtualization_role'] = ''
+
+        self.detect_virt_product('hw.product')
+        if self.facts['virtualization_type'] == '':
+            self.detect_virt_vendor('hw.vendor')
+
+        # Check the dmesg if vmm(4) attached, indicating the host is
+        # capable of virtualization.
+        dmesg_boot = get_file_content(OpenBSDVirtual.DMESG_BOOT)
+        for line in dmesg_boot.splitlines():
+            match = re.match('^vmm0 at mainbus0: (SVM/RVI|VMX/EPT)$', line)
+            if match:
+                self.facts['virtualization_type'] = 'vmm'
+                self.facts['virtualization_role'] = 'host'
