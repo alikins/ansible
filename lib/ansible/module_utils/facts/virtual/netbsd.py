b660e9ea4ae654aff7bdef5c2b854c30f0af671f
Adrian Likins <alikins@redhat.com>
2017-03-27 16:36:23 -0400

    open/net bsd virtual fact class -> fact/virtual/*bsd.py

---
+++ lib/ansible/module_utils/facts/virtual/netbsd.py
@@ -0,0 +1,21 @@
+import os
+
+from ansible.module_utils.facts import Virtual
+from ansible.module_utils.facts.virtual.sysctl import VirtualSysctlDetectionMixin
+
+
+class NetBSDVirtual(Virtual, VirtualSysctlDetectionMixin):
+    platform = 'NetBSD'
+
+    def get_virtual_facts(self):
+        # Set empty values as default
+        self.facts['virtualization_type'] = ''
+        self.facts['virtualization_role'] = ''
+
+        self.detect_virt_product('machdep.dmi.system-product')
+        if self.facts['virtualization_type'] == '':
+            self.detect_virt_vendor('machdep.dmi.system-vendor')
+
+        if os.path.exists('/dev/xencons'):
+            self.facts['virtualization_type'] = 'xen'
+            self.facts['virtualization_role'] = 'guest'
