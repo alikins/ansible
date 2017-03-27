bab152c396e601f2cb3e5aab0b294e33baed47b6
Adrian Likins <alikins@redhat.com>
2017-03-27 16:20:41 -0400

    DragonFlyVirtual -> facts/virtual/dragonfly.py

--- lib/ansible/module_utils/facts/virtual/__init__.py
+++ lib/ansible/module_utils/facts/virtual/__init__.py
@@ -1,7 +1,7 @@
 
 #from ansible.module_utils.facts.virtual import aix
 #from ansible.module_utils.facts.virtual import darwin
-#from ansible.module_utils.facts.virtual import dragonfly
+from ansible.module_utils.facts.virtual import dragonfly
 from ansible.module_utils.facts.virtual import freebsd
 #from ansible.module_utils.facts.virtual import hpux
 #from ansible.module_utils.facts.virtual import hurd
@@ -10,4 +10,4 @@ from ansible.module_utils.facts.virtual import linux
 #from ansible.module_utils.facts.virtual import openbsd
 #from ansible.module_utils.facts.virtual import sunos
 
-__all__ = [freebsd, linux]
+__all__ = [dragonfly, freebsd, linux]
