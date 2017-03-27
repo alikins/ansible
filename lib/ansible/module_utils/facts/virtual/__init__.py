93b739c1aa45476f01366323cab5f9c527f45986
Adrian Likins <alikins@redhat.com>
2017-03-27 16:15:50 -0400

    start moving Virtual fact classes to facts/virtual
    start with linux.py

---
+++ lib/ansible/module_utils/facts/virtual/__init__.py
@@ -0,0 +1,13 @@
+
+#from ansible.module_utils.facts.virtual import aix
+#from ansible.module_utils.facts.virtual import darwin
+#from ansible.module_utils.facts.virtual import dragonfly
+#from ansible.module_utils.facts.virtual import freebsd
+#from ansible.module_utils.facts.virtual import hpux
+#from ansible.module_utils.facts.virtual import hurd
+from ansible.module_utils.facts.virtual import linux
+#from ansible.module_utils.facts.virtual import netbsd
+#from ansible.module_utils.facts.virtual import openbsd
+#from ansible.module_utils.facts.virtual import sunos
+
+__all__ = [linux]
