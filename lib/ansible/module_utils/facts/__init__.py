28ca83542f8ce7df1717e89545a17f5b51a26b51
Adrian Likins <alikins@redhat.com>
2017-03-27 16:23:58 -0400

    HPUXVirtual -> facts/virtual/hpux.py

--- lib/ansible/module_utils/facts/__init__.py
+++ lib/ansible/module_utils/facts/__init__.py
@@ -1273,38 +1273,6 @@ class NetBSDVirtual(Virtual, VirtualSysctlDetectionMixin):
             self.facts['virtualization_role'] = 'guest'
 
 
-class HPUXVirtual(Virtual):
-    """
-    This is a HP-UX specific subclass of Virtual. It defines
-    - virtualization_type
-    - virtualization_role
-    """
-    platform = 'HP-UX'
-
-    def get_virtual_facts(self):
-        if os.path.exists('/usr/sbin/vecheck'):
-            rc, out, err = self.module.run_command("/usr/sbin/vecheck")
-            if rc == 0:
-                self.facts['virtualization_type'] = 'guest'
-                self.facts['virtualization_role'] = 'HP vPar'
-        if os.path.exists('/opt/hpvm/bin/hpvminfo'):
-            rc, out, err = self.module.run_command("/opt/hpvm/bin/hpvminfo")
-            if rc == 0 and re.match('.*Running.*HPVM vPar.*', out):
-                self.facts['virtualization_type'] = 'guest'
-                self.facts['virtualization_role'] = 'HPVM vPar'
-            elif rc == 0 and re.match('.*Running.*HPVM guest.*', out):
-                self.facts['virtualization_type'] = 'guest'
-                self.facts['virtualization_role'] = 'HPVM IVM'
-            elif rc == 0 and re.match('.*Running.*HPVM host.*', out):
-                self.facts['virtualization_type'] = 'host'
-                self.facts['virtualization_role'] = 'HPVM'
-        if os.path.exists('/usr/sbin/parstatus'):
-            rc, out, err = self.module.run_command("/usr/sbin/parstatus")
-            if rc == 0:
-                self.facts['virtualization_type'] = 'guest'
-                self.facts['virtualization_role'] = 'HP nPar'
-
-
 class SunOSVirtual(Virtual):
     """
     This is a SunOS-specific subclass of Virtual.  It defines
