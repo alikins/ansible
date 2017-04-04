c54c5e2518ab825eff9703a0e7cebef35672b37f
Adrian Likins <alikins@redhat.com>
2017-04-04 16:30:00 -0400

    Move Facts.is_systemd_managed to service_mgr.py
    NOTE: this will break lib/ansible/modules/system/hostname.py
    since it uses the Facts() class imported into basic.py and
    calls is_systemd_manager()

--- lib/ansible/module_utils/facts/facts.py
+++ lib/ansible/module_utils/facts/facts.py
@@ -226,17 +226,6 @@ class Facts:
                 if os.path.exists(pkg['path']):
                     self.facts['pkg_mgr'] = pkg['name']
 
-    def is_systemd_managed(self):
-        # tools must be installed
-        if self.module.get_bin_path('systemctl'):
-
-            # this should show if systemd is the boot init system, if checking init faild to mark as systemd
-            # these mirror systemd's own sd_boot test http://www.freedesktop.org/software/systemd/man/sd_booted.html
-            for canary in ["/run/systemd/system/", "/dev/.run/systemd/", "/dev/.systemd/"]:
-                if os.path.exists(canary):
-                    return True
-        return False
-
     def _get_mount_size_facts(self, mountpoint):
         size_total = None
         size_available = None
