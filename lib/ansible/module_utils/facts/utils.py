8e849879a050b33cf9630462034a617de7ed0db3
Adrian Likins <alikins@redhat.com>
2017-04-04 17:05:20 -0400

    Facts()._get_mount_size_facts() -> facts.utils.get_mount_size()
    _get_mount_size_facts doesnt need to be a method on Facts()

--- lib/ansible/module_utils/facts/utils.py
+++ lib/ansible/module_utils/facts/utils.py
@@ -38,3 +38,16 @@ def get_uname_version(module):
     if rc == 0:
         return out
     return None
+
+
+def get_mount_size(mountpoint):
+    size_total = None
+    size_available = None
+    try:
+        statvfs_result = os.statvfs(mountpoint)
+        size_total = statvfs_result.f_frsize * statvfs_result.f_blocks
+        size_available = statvfs_result.f_frsize * (statvfs_result.f_bavail)
+    except OSError:
+        pass
+
+    return size_total, size_available
