a604a6f6bffe0c0522c7ecbf722b4b2434b2b93c
Adrian Likins <alikins@redhat.com>
2017-04-05 15:23:18 -0400

    force facts_dicts.keys() to a list so py3 works

--- lib/ansible/module_utils/facts/collector.py
+++ lib/ansible/module_utils/facts/collector.py
@@ -33,7 +33,7 @@ class BaseFactCollector:
     def _transform_dict_keys(self, fact_dict):
         '''update a dicts keys to use new names as transformed by self._transform_name'''
 
-        for old_key in fact_dict.keys():
+        for old_key in list(fact_dict.keys()):
             new_key = self._transform_name(old_key)
             # pop the item by old_key and replace it using new_key
             fact_dict[new_key] = fact_dict.pop(old_key)
