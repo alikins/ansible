64b1b82a39f13fe44729d2cdec5ad32905ecbe58
Adrian Likins <alikins@redhat.com>
2017-04-07 13:09:54 -0400

    convert Facter(Facts) -> other/facter.py:FacterFactCollector

--- lib/ansible/module_utils/facts/other/facter.py
+++ lib/ansible/module_utils/facts/other/facter.py
@@ -1,4 +1,3 @@
-#
 # This file is part of Ansible
 #
 # Ansible is free software: you can redistribute it and/or modify
@@ -14,21 +13,53 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 from ansible.module_utils.facts import _json
-from ansible.module_utils.facts.facts import Facts
+
+from ansible.module_utils.facts.collector import BaseFactCollector
 
 
-class Facter(Facts):
-    """
-    This is a subclass of Facts for including information gathered from Facter.
-    """
-    def populate(self):
-        self.run_facter()
-        return self.facts
+class FacterFactCollector(BaseFactCollector):
+    _fact_ids = set(['facter'])
 
-    def run_facter(self):
+    def collect(self, collected_facts=None):
+        facter_facts = {}
+        facter_dict = {}
+
+        facter_output = self.get_facter_output()
+
+        # TODO: if we fail, should we add a empty facter key or nothing?
+        if facter_output is None:
+            return facter_facts
+
+        try:
+            facter_dict = _json.loads(facter_output)
+        except Exception:
+            # FIXME: maybe raise a FactCollectorError with some info attrs?
+            pass
+
+        # TODO: needs some munging, since facter facts end up in the main keyspace
+        facter_facts['facter'] = facter_dict
+        return facter_facts
+
+    def get_facter_output(self):
+        facter_path = self.find_facter()
+        if not facter_path:
+            return None
+
+        rc, out, err = self.run_facter(facter_path)
+
+        if rc != 0:
+            return None
+
+        return out
+
+    def find_facter(self):
         facter_path = self.module.get_bin_path('facter', opt_dirs=['/opt/puppetlabs/bin'])
         cfacter_path = self.module.get_bin_path('cfacter', opt_dirs=['/opt/puppetlabs/bin'])
+
         # Prefer to use cfacter if available
         if cfacter_path is not None:
             facter_path = cfacter_path
@@ -36,10 +67,10 @@ class Facter(Facts):
         if facter_path is None:
             return
 
+        return facter_path
+
+    def run_facter(self, facter_path):
         # if facter is installed, and we can use --json because
         # ruby-json is ALSO installed, include facter data in the JSON
         rc, out, err = self.module.run_command(facter_path + " --puppet --json")
-        try:
-            self.facts = _json.loads(out)
-        except:
-            pass
+        return rc, out, err
