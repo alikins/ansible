f8a2dde8d93f17c93d16403abfda3d345a08477e
Adrian Likins <alikins@redhat.com>
2017-04-07 12:11:59 -0400

    mv lsb fact tests to {tests}/facts/system/test_lsb.py

--- test/units/module_utils/test_facts.py
+++ test/units/module_utils/test_facts.py
@@ -32,8 +32,6 @@ from ansible.module_utils.facts import network
 from ansible.module_utils.facts import virtual
 from ansible.module_utils.facts.virtual.linux import LinuxVirtual
 
-from ansible.module_utils.facts.system.lsb import LSBFactCollector
-
 from .facts.base import BaseFactsTest
 
 
@@ -43,88 +41,6 @@ class TestFacterCollector(BaseFactsTest):
     valid_subsets = ['facter']
     fact_namespace = 'ansible_facter'
     collector_class = facts.FacterCollector
-
-
-lsb_release_a_fedora_output = '''
-LSB Version:	:core-4.1-amd64:core-4.1-noarch:cxx-4.1-amd64:cxx-4.1-noarch:desktop-4.1-amd64:desktop-4.1-noarch:languages-4.1-amd64:languages-4.1-noarch:printing-4.1-amd64:printing-4.1-noarch
-Distributor ID:	Fedora
-Description:	Fedora release 25 (Twenty Five)
-Release:	25
-Codename:	TwentyFive
-''' # noqa
-
-# FIXME: a
-etc_lsb_release_ubuntu14 = '''DISTRIB_ID=Ubuntu
-DISTRIB_RELEASE=14.04
-DISTRIB_CODENAME=trusty
-DISTRIB_DESCRIPTION="Ubuntu 14.04.3 LTS"
-'''
-etc_lsb_release_no_decimal = '''DISTRIB_ID=AwesomeOS
-DISTRIB_RELEASE=11
-DISTRIB_CODENAME=stonehenge
-DISTRIB_DESCRIPTION="AwesomeÖS 11"
-'''
-
-
-class TestLSBFacts(BaseFactsTest):
-    __test__ = True
-    gather_subset = ['!all', 'lsb']
-    valid_subsets = ['lsb']
-    fact_namespace = 'ansible_lsb'
-    collector_class = LSBFactCollector
-
-    def _mock_module(self):
-        mock_module = Mock()
-        mock_module.params = {'gather_subset': self.gather_subset,
-                              'gather_timeout': 10,
-                              'filter': '*'}
-        mock_module.get_bin_path = Mock(return_value='/usr/bin/lsb_release')
-        mock_module.run_command = Mock(return_value=(0, lsb_release_a_fedora_output, ''))
-        return mock_module
-
-    def test_lsb_release_bin(self):
-        module = self._mock_module()
-        fact_collector = self.collector_class(module=module)
-        facts_dict = fact_collector.collect()
-
-        self.assertIsInstance(facts_dict, dict)
-        self.assertEqual(facts_dict['lsb']['release'], '25')
-        self.assertEqual(facts_dict['lsb']['id'], 'Fedora')
-        self.assertEqual(facts_dict['lsb']['description'], 'Fedora release 25 (Twenty Five)')
-        self.assertEqual(facts_dict['lsb']['codename'], 'TwentyFive')
-        self.assertEqual(facts_dict['lsb']['major_release'], '25')
-
-    def test_etc_lsb_release(self):
-        module = self._mock_module()
-        module.get_bin_path = Mock(return_value=None)
-        with patch('ansible.module_utils.facts.system.lsb.os.path.exists',
-                   return_value=True):
-            with patch('ansible.module_utils.facts.system.lsb.get_file_lines',
-                       return_value=etc_lsb_release_ubuntu14.splitlines()):
-                fact_collector = self.collector_class(module=module)
-                facts_dict = fact_collector.collect()
-
-        self.assertIsInstance(facts_dict, dict)
-        self.assertEqual(facts_dict['lsb']['release'], '14.04')
-        self.assertEqual(facts_dict['lsb']['id'], 'Ubuntu')
-        self.assertEqual(facts_dict['lsb']['description'], '"Ubuntu 14.04.3 LTS"')
-        self.assertEqual(facts_dict['lsb']['codename'], 'trusty')
-
-    def test_etc_lsb_release_no_decimal_release(self):
-        module = self._mock_module()
-        module.get_bin_path = Mock(return_value=None)
-        with patch('ansible.module_utils.facts.system.lsb.os.path.exists',
-                   return_value=True):
-            with patch('ansible.module_utils.facts.system.lsb.get_file_lines',
-                       return_value=etc_lsb_release_no_decimal.splitlines()):
-                fact_collector = self.collector_class(module=module)
-                facts_dict = fact_collector.collect()
-
-        self.assertIsInstance(facts_dict, dict)
-        self.assertEqual(facts_dict['lsb']['release'], '11')
-        self.assertEqual(facts_dict['lsb']['id'], 'AwesomeOS')
-        self.assertEqual(facts_dict['lsb']['description'], '"AwesomeÖS 11"')
-        self.assertEqual(facts_dict['lsb']['codename'], 'stonehenge')
 
 
 class BaseTestFactsPlatform(unittest.TestCase):
