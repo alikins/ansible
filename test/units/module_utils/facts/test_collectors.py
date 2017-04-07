50185c2576797394b870766b7a0a5f55fb3882a7
Adrian Likins <alikins@redhat.com>
2017-04-07 12:08:01 -0400

    split fact collector tests to test_collectors.py
    will eventually split these up further

---
+++ test/units/module_utils/facts/test_collectors.py
@@ -0,0 +1,196 @@
+# unit tests for ansible fact collectors
+# -*- coding: utf-8 -*-
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Make coding more python3-ish
+from __future__ import (absolute_import, division)
+__metaclass__ = type
+
+from ansible.compat.tests.mock import Mock, patch
+
+from . base import BaseFactsTest
+
+from ansible.module_utils.facts.system.apparmor import ApparmorFactCollector
+from ansible.module_utils.facts.system.env import EnvFactCollector
+from ansible.module_utils.facts.system.fips import FipsFactCollector
+from ansible.module_utils.facts.system.pkg_mgr import PkgMgrFactCollector
+from ansible.module_utils.facts.system.selinux import SelinuxFactCollector
+from ansible.module_utils.facts.system.service_mgr import ServiceMgrFactCollector
+from ansible.module_utils.facts.system.caps import SystemCapabilitiesFactCollector
+
+
+class TestApparmorFacts(BaseFactsTest):
+    __test__ = True
+    gather_subset = ['!all', 'apparmor']
+    valid_subsets = ['apparmor']
+    fact_namespace = 'ansible_apparmor'
+    collector_class = ApparmorFactCollector
+
+    def test_class(self):
+        facts_dict = super(TestApparmorFacts, self).test_class()
+        self.assertIn('status', facts_dict['apparmor'])
+
+
+class TestCollectedCapsFacts(BaseFactsTest):
+    __test__ = True
+    gather_subset = ['!all', 'caps']
+    valid_subsets = ['caps']
+    fact_namespace = 'ansible_system_capabilities'
+    collector_class = SystemCapabilitiesFactCollector
+
+    def _mock_module(self):
+        mock_module = Mock()
+        mock_module.params = {'gather_subset': self.gather_subset,
+                              'gather_timeout': 10,
+                              'filter': '*'}
+        mock_module.get_bin_path = Mock(return_value='/usr/sbin/capsh')
+        mock_module.run_command = Mock(return_value=(0, 'Current: =ep', ''))
+        return mock_module
+
+
+class TestEnvFacts(BaseFactsTest):
+
+    __test__ = True
+    gather_subset = ['!all', 'env']
+    valid_subsets = ['env']
+    fact_namespace = 'ansible_env'
+    collector_class = EnvFactCollector
+
+    def test_class(self):
+        facts_dict = super(TestEnvFacts, self).test_class()
+        self.assertIn('HOME', facts_dict['env'])
+
+
+class TestCollectedFipsFacts(BaseFactsTest):
+    __test__ = True
+    gather_subset = ['!all', 'fips']
+    valid_subsets = ['fips']
+    fact_namespace = 'ansible_fips'
+    collector_class = FipsFactCollector
+
+
+class TestPkgMgrFacts(BaseFactsTest):
+    __test__ = True
+    gather_subset = ['!all', 'pkg_mgr']
+    valid_subsets = ['pkg_mgr']
+    fact_namespace = 'ansible_pkgmgr'
+    collector_class = PkgMgrFactCollector
+
+
+class TestSelinuxFacts(BaseFactsTest):
+    __test__ = True
+    gather_subset = ['!all', 'selinux']
+    valid_subsets = ['selinux']
+    fact_namespace = 'ansible_selinux'
+    collector_class = SelinuxFactCollector
+
+    def test_no_selinux(self):
+        with patch('ansible.module_utils.facts.system.selinux.HAVE_SELINUX', False):
+            module = self._mock_module()
+            fact_collector = self.collector_class(module=module)
+            facts_dict = fact_collector.collect()
+            self.assertIsInstance(facts_dict, dict)
+            self.assertFalse(facts_dict['selinux'])
+            return facts_dict
+
+
+class TestServiceMgrFacts(BaseFactsTest):
+    __test__ = True
+    gather_subset = ['!all', 'service_mgr']
+    valid_subsets = ['service_mgr']
+    fact_namespace = 'ansible_service_mgr'
+    collector_class = ServiceMgrFactCollector
+
+    # TODO: dedupe some of this test code
+
+    @patch('ansible.module_utils.facts.system.service_mgr.get_file_content', return_value=None)
+    def test_no_proc1(self, mock_gfc):
+        # no /proc/1/comm, ps returns non-0
+        # should fallback to 'service'
+        module = self._mock_module()
+        module.run_command = Mock(return_value=(1, '', 'wat'))
+        fact_collector = self.collector_class(module=module)
+        facts_dict = fact_collector.collect()
+        self.assertIsInstance(facts_dict, dict)
+        self.assertEqual(facts_dict['service_mgr'], 'service')
+
+    @patch('ansible.module_utils.facts.system.service_mgr.get_file_content', return_value=None)
+    def test_no_proc1_ps_random_init(self, mock_gfc):
+        # no /proc/1/comm, ps returns '/sbin/sys11' which we dont know
+        # should end up return 'sys11'
+        module = self._mock_module()
+        module.run_command = Mock(return_value=(0, '/sbin/sys11', ''))
+        fact_collector = self.collector_class(module=module)
+        facts_dict = fact_collector.collect()
+        self.assertIsInstance(facts_dict, dict)
+        self.assertEqual(facts_dict['service_mgr'], 'sys11')
+
+    @patch('ansible.module_utils.facts.system.service_mgr.get_file_content', return_value=None)
+    def test_clowncar(self, mock_gfc):
+        # no /proc/1/comm, ps fails, distro and system are clowncar
+        # should end up return 'sys11'
+        module = self._mock_module()
+        module.run_command = Mock(return_value=(1, '', ''))
+        collected_facts = {'distribution': 'clowncar',
+                           'system': 'ClownCarOS'}
+        fact_collector = self.collector_class(module=module)
+        facts_dict = fact_collector.collect(collected_facts=collected_facts)
+        self.assertIsInstance(facts_dict, dict)
+        self.assertEqual(facts_dict['service_mgr'], 'service')
+
+    # TODO: reenable these tests when we can mock more easily
+
+#    @patch('ansible.module_utils.facts.system.service_mgr.get_file_content', return_value=None)
+#    def test_sunos_fallback(self, mock_gfc):
+#        # no /proc/1/comm, ps fails, 'system' is SunOS
+#        # should end up return 'smf'?
+#        module = self._mock_module()
+#        # FIXME: the result here is a kluge to at least cover more of service_mgr.collect
+#        # TODO: remove
+#        # FIXME: have to force a pid for results here to get into any of the system/distro checks
+#        module.run_command = Mock(return_value=(1, ' 37 ', ''))
+#        collected_facts = {'system': 'SunOS'}
+#        fact_collector = self.collector_class(module=module)
+#        facts_dict = fact_collector.collect(collected_facts=collected_facts)
+#        print('facts_dict: %s' % facts_dict)
+#        self.assertIsInstance(facts_dict, dict)
+#        self.assertEqual(facts_dict['service_mgr'], 'smf')
+
+#    @patch('ansible.module_utils.facts.system.service_mgr.get_file_content', return_value=None)
+#    def test_aix_fallback(self, mock_gfc):
+#        # no /proc/1/comm, ps fails, 'system' is SunOS
+#        # should end up return 'smf'?
+#        module = self._mock_module()
+#        module.run_command = Mock(return_value=(1, '', ''))
+#        collected_facts = {'system': 'AIX'}
+#        fact_collector = self.collector_class(module=module)
+#        facts_dict = fact_collector.collect(collected_facts=collected_facts)
+#        print('facts_dict: %s' % facts_dict)
+#        self.assertIsInstance(facts_dict, dict)
+#        self.assertEqual(facts_dict['service_mgr'], 'src')
+
+#    @patch('ansible.module_utils.facts.system.service_mgr.get_file_content', return_value=None)
+#    def test_linux_fallback(self, mock_gfc):
+#        # no /proc/1/comm, ps fails, 'system' is SunOS
+#        # should end up return 'smf'?
+#        module = self._mock_module()
+#        module.run_command = Mock(return_value=(1, '  37 ', ''))
+#        collected_facts = {'system': 'Linux'}
+#        fact_collector = self.collector_class(module=module)
+#        facts_dict = fact_collector.collect(collected_facts=collected_facts)
+#        print('facts_dict: %s' % facts_dict)
+#        self.assertIsInstance(facts_dict, dict)
+#        self.assertEqual(facts_dict['service_mgr'], 'sdfadf')
---
+++ test/units/module_utils/facts/test_init.py
@@ -0,0 +1,199 @@
+# unit tests for ansible/module_utils/facts/__init__.py
+# -*- coding: utf-8 -*-
+#
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Make coding more python3-ish
+from __future__ import (absolute_import, division)
+__metaclass__ = type
+
+import re
+
+# for testing
+from ansible.compat.tests import unittest
+from ansible.compat.tests.mock import Mock
+
+# module under test
+from ansible.module_utils import facts
+
+
+# FIXME: this is brute force, but hopefully enough to get some refactoring to make facts testable
+class TestInPlace(unittest.TestCase):
+    def _mock_module(self):
+        mock_module = Mock()
+        mock_module.params = {'gather_subset': ['all', '!facter', '!ohai'],
+                              'gather_timeout': 5,
+                              'filter': '*'}
+        mock_module.get_bin_path = Mock(return_value=None)
+        return mock_module
+
+    def test(self):
+        mock_module = self._mock_module()
+        fact_collector = facts.AnsibleFactCollector.from_gather_subset(mock_module,
+                                                                       gather_subset=['all'])
+        res = fact_collector.collect()
+        self.assertIsInstance(res, dict)
+        self.assertIn('ansible_facts', res)
+        # just assert it's not almost empty
+        self.assertGreater(len(res['ansible_facts']), 30)
+
+    def test_collect_ids(self):
+        mock_module = self._mock_module()
+        fact_collector = facts.AnsibleFactCollector.from_gather_subset(mock_module,
+                                                                       gather_subset=['all'])
+        res = fact_collector.collect_ids()
+
+        self.assertIsInstance(res, set)
+
+    def test_collect_ids_minimal(self):
+        mock_module = self._mock_module()
+        gather_subset = ['!all']
+        mock_module.params['gather_subset'] = gather_subset
+
+        fact_collector = facts.AnsibleFactCollector.from_gather_subset(mock_module,
+                                                                       gather_subset=gather_subset)
+        res = fact_collector.collect_ids()
+
+        self.assertIsInstance(res, set)
+        not_expected_facts = ['facter', 'lsb', 'virtual']
+        for not_expected_fact in not_expected_facts:
+            self.assertNotIn(not_expected_fact, res)
+
+    def test_facts_class(self):
+        mock_module = self._mock_module()
+        facts.Facts(mock_module)
+
+    def test_facts_class_load_on_init_false(self):
+        mock_module = self._mock_module()
+        facts.Facts(mock_module, load_on_init=False)
+        # FIXME: assert something
+
+    def test_facts_class_populate(self):
+        mock_module = self._mock_module()
+        facts_obj = facts.Facts(mock_module)
+        res = facts_obj.populate()
+        self.assertIsInstance(res, dict)
+        self.assertIn('python_version', res)
+        # just assert it's not almost empty
+        self.assertGreater(len(res), 5)
+
+
+class TestCollectedFacts(unittest.TestCase):
+    gather_subset = ['all', '!facter', '!ohai']
+    min_fact_count = 30
+    max_fact_count = 1000
+    expected_facts = ['ansible_cmdline', 'ansible_date_time',
+                      'ansible_user_id', 'ansible_distribution',
+                      'ansible_gather_subset', 'module_setup',
+                      'ansible_env',
+                      'ansible_ssh_host_key_rsa_public']
+    not_expected_facts = ['facter', 'ohai']
+
+    def _mock_module(self):
+        mock_module = Mock()
+        mock_module.params = {'gather_subset': self.gather_subset,
+                              'gather_timeout': 5,
+                              'filter': '*'}
+        mock_module.get_bin_path = Mock(return_value=None)
+        return mock_module
+
+    def setUp(self):
+        mock_module = self._mock_module()
+        fact_collector = facts.AnsibleFactCollector.from_gather_subset(mock_module,
+                                                                       gather_subset=self.gather_subset)
+        self.facts = fact_collector.collect()
+
+    def test_basics(self):
+        self._assert_basics(self.facts)
+
+    def test_expected_facts(self):
+        self._assert_expected_facts(self.facts)
+
+    def test_not_expected_facts(self):
+        self._assert_not_expected_facts(self.facts)
+
+    def test_has_ansible_namespace(self):
+        self._assert_ansible_namespace(self.facts)
+
+    def test_no_ansible_dupe_in_key(self):
+        self._assert_no_ansible_dupe(self.facts)
+
+    def _assert_basics(self, facts):
+        self.assertIsInstance(facts, dict)
+        self.assertIn('ansible_facts', facts)
+        # just assert it's not almost empty
+        self.assertGreater(len(facts['ansible_facts']), self.min_fact_count)
+        # and that is not huge number of keys
+        self.assertLess(len(facts['ansible_facts']), self.max_fact_count)
+
+    # everything starts with ansible_ namespace
+    def _assert_ansible_namespace(self, facts):
+        subfacts = facts['ansible_facts']
+
+        # FIXME: kluge for non-namespace fact
+        subfacts.pop('module_setup', None)
+
+        for fact_key in subfacts:
+            self.assertTrue(fact_key.startswith('ansible_'),
+                            'The fact name "%s" does not startwith "ansible_"' % fact_key)
+
+    # verify that we only add one 'ansible_' namespace
+    def _assert_no_ansible_dupe(self, facts):
+        subfacts = facts['ansible_facts']
+        re_ansible = re.compile('ansible')
+        re_ansible_underscore = re.compile('ansible_')
+
+        # FIXME: kluge for non-namespace fact
+        subfacts.pop('module_setup', None)
+
+        for fact_key in subfacts:
+            ansible_count = re_ansible.findall(fact_key)
+            self.assertEqual(len(ansible_count), 1,
+                             'The fact name "%s" should have 1 "ansible" substring in it.' % fact_key)
+            ansible_underscore_count = re_ansible_underscore.findall(fact_key)
+            self.assertEqual(len(ansible_underscore_count), 1,
+                             'The fact name "%s" should have 1 "ansible_" substring in it.' % fact_key)
+
+    def _assert_expected_facts(self, facts):
+        subfacts = facts['ansible_facts']
+
+        subfacts_keys = sorted(subfacts.keys())
+        for expected_fact in self.expected_facts:
+            self.assertIn(expected_fact, subfacts_keys)
+        #    self.assertIsInstance(subfacts['ansible_env'], dict)
+
+        # self.assertIsInstance(subfacts['ansible_env'], dict)
+
+        # self._assert_ssh_facts(subfacts)
+
+    def _assert_not_expected_facts(self, facts):
+        subfacts = facts['ansible_facts']
+
+        subfacts_keys = sorted(subfacts.keys())
+        for not_expected_fact in self.not_expected_facts:
+            self.assertNotIn(not_expected_fact, subfacts_keys)
+
+    def _assert_ssh_facts(self, subfacts):
+        self.assertIn('ansible_ssh_host_key_rsa_public', subfacts.keys())
+
+
+class TestMinimalCollectedFacts(TestCollectedFacts):
+    gather_subset = ['!all']
+    min_fact_count = 1
+    max_fact_count = 10
+    expected_facts = ['ansible_gather_subset',
+                      'module_setup']
+    not_expected_facts = ['ansible_lsb']
