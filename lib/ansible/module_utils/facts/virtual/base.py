c34643d252fde74f5ec021eee20956ca0745f26c
Adrian Likins <alikins@redhat.com>
2017-04-07 15:22:17 -0400

    start moving virt facts to collector

--- lib/ansible/module_utils/facts/virtual/base.py
+++ lib/ansible/module_utils/facts/virtual/base.py
@@ -1,3 +1,21 @@
+# base classes for virtualization facts
+# -*- coding: utf-8 -*-
+#
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
@@ -7,6 +25,8 @@ from ansible.module_utils.basic import get_all_subclasses
 from ansible.module_utils.six import PY3
 
 from ansible.module_utils.facts.facts import Facts
+
+from ansible.module_utils.facts.collector import BaseFactCollector
 
 
 class Virtual(Facts):
@@ -45,3 +65,17 @@ class Virtual(Facts):
     def get_virtual_facts(self):
         self.facts['virtualization_type'] = ''
         self.facts['virtualization_role'] = ''
+
+
+class VirtualCollector(BaseFactCollector):
+    _fact_ids = set(['virtual',
+                     'virtualization_type', 'virtualization_role'])
+
+    def collect(self, collected_facts=None):
+        collected_facts = collected_facts or {}
+
+        virtual_facts = Virtual(self.module, cached_facts=collected_facts.copy())
+
+        facts_dict = virtual_facts.populate()
+
+        return facts_dict
