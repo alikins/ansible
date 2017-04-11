e58141187b47a10f0270d2cebbb07303c8ba0f36
Adrian Likins <alikins@redhat.com>
2017-04-11 16:25:09 -0400

    update platform.py to not mutate self.facts

--- lib/ansible/module_utils/facts/system/platform.py
+++ lib/ansible/module_utils/facts/system/platform.py
@@ -38,48 +38,49 @@ class PlatformFactCollector(BaseFactCollector):
                      'machine_id'])
 
     def collect(self, collected_facts=None):
+        platform_facts = {}
         # Platform
         # platform.system() can be Linux, Darwin, Java, or Windows
         # NOTE: pretty much every method should create a new dict (or whatever the FactsModel ds is)
         #       and return it and let main Facts() class combine them. -akl
         # NOTE: a facts.Platform() class that wraps all of this would make mocking/testing easier -akl
-        self.facts['system'] = platform.system()
-        self.facts['kernel'] = platform.release()
-        self.facts['machine'] = platform.machine()
+        platform_facts['system'] = platform.system()
+        platform_facts['kernel'] = platform.release()
+        platform_facts['machine'] = platform.machine()
 
         # move to system/python.py?
-        self.facts['python_version'] = platform.python_version()
+        platform_facts['python_version'] = platform.python_version()
 
         # NOTE: not platform at all... -akl
-        self.facts['fqdn'] = socket.getfqdn()
-        self.facts['hostname'] = platform.node().split('.')[0]
-        self.facts['nodename'] = platform.node()
+        platform_facts['fqdn'] = socket.getfqdn()
+        platform_facts['hostname'] = platform.node().split('.')[0]
+        platform_facts['nodename'] = platform.node()
 
         # NOTE: not platform -akl
-        self.facts['domain'] = '.'.join(self.facts['fqdn'].split('.')[1:])
+        platform_facts['domain'] = '.'.join(platform_facts['fqdn'].split('.')[1:])
 
         arch_bits = platform.architecture()[0]
 
         # NOTE: this could be split into arch and/or system specific classes/methods -akl
-        self.facts['userspace_bits'] = arch_bits.replace('bit', '')
-        if self.facts['machine'] == 'x86_64':
-            self.facts['architecture'] = self.facts['machine']
-            if self.facts['userspace_bits'] == '64':
-                self.facts['userspace_architecture'] = 'x86_64'
-            elif self.facts['userspace_bits'] == '32':
-                self.facts['userspace_architecture'] = 'i386'
-        elif solaris_i86_re.search(self.facts['machine']):
-            self.facts['architecture'] = 'i386'
-            if self.facts['userspace_bits'] == '64':
-                self.facts['userspace_architecture'] = 'x86_64'
-            elif self.facts['userspace_bits'] == '32':
-                self.facts['userspace_architecture'] = 'i386'
+        platform_facts['userspace_bits'] = arch_bits.replace('bit', '')
+        if platform_facts['machine'] == 'x86_64':
+            platform_facts['architecture'] = platform_facts['machine']
+            if platform_facts['userspace_bits'] == '64':
+                platform_facts['userspace_architecture'] = 'x86_64'
+            elif platform_facts['userspace_bits'] == '32':
+                platform_facts['userspace_architecture'] = 'i386'
+        elif solaris_i86_re.search(platform_facts['machine']):
+            platform_facts['architecture'] = 'i386'
+            if platform_facts['userspace_bits'] == '64':
+                platform_facts['userspace_architecture'] = 'x86_64'
+            elif platform_facts['userspace_bits'] == '32':
+                platform_facts['userspace_architecture'] = 'i386'
         else:
-            self.facts['architecture'] = self.facts['machine']
+            platform_facts['architecture'] = platform_facts['machine']
 
         # FIXME: as much as possible, avoid arch/platform bits here
         # NOTE: -> aix_platform = AixPlatform(); facts_dict.update(aix_platform) -akl
-        if self.facts['system'] == 'AIX':
+        if platform_facts['system'] == 'AIX':
             # Attempt to use getconf to figure out architecture
             # fall back to bootinfo if needed
             # NOTE: in general, the various 'get_bin_path(); data=run_command()' could be split to methods/classes for providing info
@@ -89,18 +90,20 @@ class PlatformFactCollector(BaseFactCollector):
             if getconf_bin:
                 rc, out, err = self.module.run_command([getconf_bin, 'MACHINE_ARCHITECTURE'])
                 data = out.splitlines()
-                self.facts['architecture'] = data[0]
+                platform_facts['architecture'] = data[0]
             else:
                 bootinfo_bin = self.module.get_bin_path('bootinfo')
                 rc, out, err = self.module.run_command([bootinfo_bin, '-p'])
                 data = out.splitlines()
-                self.facts['architecture'] = data[0]
-        elif self.facts['system'] == 'OpenBSD':
-            self.facts['architecture'] = platform.uname()[5]
+                platform_facts['architecture'] = data[0]
+        elif platform_facts['system'] == 'OpenBSD':
+            platform_facts['architecture'] = platform.uname()[5]
 
         # NOTE: the same comment about get_bin_path() above also applies to fetching file content
         #       attempting to mock a file open and read is a PITA, but mocking read_dbus_machine_id() is easy to mock -akl
         machine_id = get_file_content("/var/lib/dbus/machine-id") or get_file_content("/etc/machine-id")
         if machine_id:
             machine_id = machine_id.splitlines()[0]
-            self.facts["machine_id"] = machine_id
+            platform_facts["machine_id"] = machine_id
+
+        return platform_facts
