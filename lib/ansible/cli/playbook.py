3c429337f0f94b93bab21acda45a7829060e7d79
Adrian Likins <alikins@redhat.com>
2017-12-14 16:29:38 -0500

    merged fixups

--- lib/ansible/cli/playbook.py
+++ lib/ansible/cli/playbook.py
@@ -39,7 +39,7 @@ except ImportError:
     display = Display()
 
 SKIP = True
-#---------------------------------------------------------------------------------------------------
+
 
 class PlaybookCLI(CLI):
     ''' the tool to run *Ansible playbooks*, which are a configuration and multinode deployment system.
@@ -246,9 +246,6 @@ class PlaybookCLI(CLI):
                            max_depth=5)
         objgraph.show_backrefs(objs,
                                refcounts=True,
-                               shortnames=False,
-                               filename=backrefs_full_fn,
-                               max_depth=5)
                                shortnames=False,
                                filename=backrefs_full_fn,
                                max_depth=5)
