fcdc7714460e03d7559d1fed3e2b16b6c8a670bb
Adrian Likins <alikins@redhat.com>
2018-02-02 10:45:03 -0500

    some tweaks to the test-galaxy-install.sh

--- test-galaxy-install.sh
+++ test-galaxy-install.sh
@@ -9,6 +9,12 @@ display() {
     printf "##### TEST: %s\n" "${@}"
 }
 
+ANSIBLE_CONFIG=/home/adrian/src/ansible/ansible_empty.cfg
+ANSIBLE_ROLES_PATH="/tmp/test-galaxy-install/roles"
+export ANSIBLE_CONFIG
+export ANSIBLE_ROLES_PATH
+mkdir -p "${ANSIBLE_ROLES_PATH}"
+
 verbosity="-vvvvv"
 
 cleanup
@@ -17,23 +23,29 @@ display "legacy role from git+https"
 
 ansible-galaxy content-install git+https://github.com/geerlingguy/ansible-role-ansible.git $verbosity
 
+ansible-galaxy list
+
 cleanup
 
 display "legacy role from galaxy"
 
 ansible-galaxy content-install geerlingguy.ansible $verbosity
-
+ansible-galaxy list
 cleanup
 
 display "legacy role from galaxy with dependencies"
 
 ansible-galaxy content-install hxpro.nginx $verbosity
-
+ansible-galaxy list
 cleanup
 
 display "modules from git+https WITHOUT galaxyfile"
 
 ansible-galaxy content-install -t module git+https://github.com/maxamillion/test-galaxy-content $verbosity
+ansible-galaxy list
+
+#ansible-doc my_sample_module
+ansible "${verbosity}" localhost -m my_sample_module -a 'name=foooo1'
 
 cleanup
 
@@ -46,5 +58,10 @@ cleanup
 display "all content git+https WITH galaxyfile"
 
 ansible-galaxy content-install git+https://github.com/maxamillion/test-galaxy-content-galaxyfile $verbosity
+ansible-galaxy list
+# ansible-doc galaxyfile_sample_module
+# ansible-doc module_c
+
+ansible "${verbosity}" localhost -m galaxyfile_sample_module -a 'name=bar1'
 
 cleanup
