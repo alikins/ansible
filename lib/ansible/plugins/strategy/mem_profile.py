99c8b55c64ff4dd8870e7c07dd56adffbcba10f0
Ansible Test Runner <noreply@example.com>
2018-02-06 16:42:43 -0500

    mem_profile for max-bug-repro

--- lib/ansible/plugins/strategy/mem_profile.py
+++ lib/ansible/plugins/strategy/mem_profile.py
@@ -18,10 +18,11 @@ __metaclass__ = type
 import os
 
 from ansible.plugins.strategy.linear import StrategyModule as LinearStrategyModule
-from ansible.module_utils.six import string_types
 
 import objgraph
 import memory_profiler as mem_profile
+import pprint
+import threading
 
 DOCUMENTATION = '''
     strategy: mem_profile
@@ -51,15 +52,18 @@ def show_table(stats):
 
 def filter_obj(obj):
     try:
-        if not obj.__class__.__module__.startswith('ansible'):
-            return False
+        module_name = obj.__class__.__module__
+        #not_these = ('AnsibleUnicode', 'AnsibleMapping')
+        not_these = []
+        if module_name.startswith('ansible'):
+            return True
+        #if not obj.__class__.__module__.startswith('ansible'):
+        #    return False
     except Exception as e:
         print(e)
-    return True
 
-
-def filter_str(obj):
-    if isinstance(obj, string_types):
+    type_name = obj.__class__.__name__
+    if type_name not in ('__builtin__.dict', '__builtin__.list'):
         return False
     return True
 
@@ -79,15 +83,25 @@ def extra_info_repr(obj):
 
 def extra_info_id(obj):
     '''return the hex obj id as extra_info'''
-
     return hex(id(obj))
+
+
+def extra_info_id_and_pid(obj):
+    return repr((hex(id(obj)), os.getpid()))
 
 
 def show_common_ansible_types(limit=None):
     print('\nmost common ansible types:')
     common = objgraph.most_common_types(shortnames=False, limit=limit)
-    ans_stats = [x for x in common if x[0].startswith('ansible') and x[1] > 1]
+    # ans_stats = [x for x in common if x[0].startswith('ansible') and x[1] > 1]
+    ans_stats = [x for x in common if x[0].startswith('ansible') and x[1] > 10]
     show_table(ans_stats)
+
+
+def show_common_types(limit=None):
+    print('\nmost common types:')
+    common = objgraph.most_common_types(shortnames=False, limit=limit)
+    show_table(common)
 
 
 # TODO/FIXME: make decorator
@@ -114,10 +128,15 @@ def track_mem(msg=None, pid=None, call_stack=None, subsystem=None, prev_mem=None
         print('MEM change: %s MiB cur: %s prev: %s (pid=%s) %s -- %s' %
               (delta, new_mem, prev_mem, pid, subsystem, msg))
 
-        print('new objects:')
-        objgraph.show_growth(limit=30, shortnames=False)
+        growth = objgraph.growth(limit=30, shortnames=False)
+        print('growth')
+        pprint.pprint(growth)
 
-        show_common_ansible_types(limit=2000)
+        #print('new objects:')
+        #objgraph.show_growth(limit=30, shortnames=False)
+
+        #show_common_ansible_types(limit=2000)
+        #show_common_types(limit=2000)
         print('\n')
 
     return prev_mem
@@ -133,24 +152,56 @@ def show_refs(filename=None, objs=None, max_depth=5, max_objs=None):
     if max_objs:
         objs = objs[:max_objs]
 
-    objgraph.show_refs(objs,
-                       filename=refs_full_fn,
-                       refcounts=True,
-                       extra_info=extra_info_id,
-                       shortnames=False,
-                       #filter=filter_str,
-                       too_many=4,
-                       max_depth=max_depth)
+    def filter_obj2(obj):
+        return not objgraph.is_proper_module(obj)
 
-    objgraph.show_backrefs(objs,
-                           refcounts=True,
-                           shortnames=False,
-                           extra_info=extra_info_id,
-                           filename=backrefs_full_fn,
-                           too_many=4,
-                           #filter=filter_str,
-                           max_depth=max_depth)
+    # return
+#    print('\nbackrefs for %s pid=%s' % (objs, os.getpid()))
+    for obj in objs:
+        #print(obj)
+        continue
+        pprint.pprint(obj.__dict__)
+        res = obj.__dict__.get('_result', None)
+        if res:
+            pprint.pprint(res)
+            for i in res:
+                print('%s (%s): %s' % (i, type(res[i]), res[i]))
 
+            #for i in res:
+            #    print('%s (%s): %s' % (i, type(res[i]), res[i]))
+        # pprint.pprint(obj.__dict__)
+        #backrefs = objgraph.find_backref_chain(obj, filter_obj)
+        #for backref in backrefs:
+        #    pprint.pprint(backref)
+        #print('refs')
+        #refs = objgraph.find_ref_chain(obj, filter_obj)
+        #for ref in refs:
+        #    pprint.pprint(ref)
+        #print('\n')
+
+
+    #if False:
+    if True:
+        objgraph.show_refs(objs,
+                        filename=refs_full_fn,
+                        refcounts=True,
+                        extra_info=extra_info_id_and_pid,
+                        shortnames=False,
+                        #filter=filter_obj,
+                        max_depth=max_depth)
+    #
+        objgraph.show_backrefs(objs,
+                            refcounts=True,
+                            shortnames=False,
+                            extra_info=extra_info_id_and_pid,
+                            filename=backrefs_full_fn,
+                            #filter=filter_obj,
+                            max_depth=max_depth)
+
+        # import sys
+        # sys.exit()
+
+import gc
 
 class StrategyModule(LinearStrategyModule):
     def __init__(self, tqm):
@@ -158,38 +209,61 @@ class StrategyModule(LinearStrategyModule):
         self.prev_mem = 0
         self.track_mem(msg='in __init__')
 
+        # gc.set_debug(gc.DEBUG_LEAK|gc.DEBUG_STATS)
+
+    def show_backrefs(self):
+        # funcs = objgraph.by_type('__builtin__.dict')
+        # funcs = objgraph.by_type('ansible.parsing.yaml.objects.AnsibleUnicode')
+        #funcs = objgraph.by_type('ansible.executor.task_result.TaskResult')
+
+        funcs = []
+        #funcs = objgraph.by_type('ansible.playbook.task.Task')
+        #funcs = objgraph.by_type('ansible.vars.manager.VariableManager')
+        funcs = objgraph.by_type('ansible.vars.hostvars.HostVars')
+        #funcs.extend(objgraph.by_type('ansible.playbook.block.Block'))
+        #funcs.extend(objgraph.by_type('TaskResult'))
+        #funcs.extend(objgraph.by_type('ansible.playbook.role_include.RoleInclude'))
+        #funcs.extend(objgraph.by_type('ansible.playbook.included_file.IncludedFile'))
+        #funcs.extend(objgraph.by_type('ansible.playbook.base.Base'))
+        #funcs.extend(objgraph.by_type('ansible.playbook.handler.Handler'))
+        #funcs.extend(objgraph.by_type('ansible.inventory.host.Host'))
+        #funcs.extend(objgraph.by_type('ansible.inventory.group.Group'))
+        if len(funcs) > 0:
+
+#        show_refs(filename='task_include_refs', objs=tis, max_depth=6, max_objs=1)
+            show_refs(filename='task_include_refs', objs=funcs, max_depth=6)
+
+
     def track_mem(self, msg=None, pid=None, call_stack=None, subsystem=None):
         subsystem = subsystem or 'strategy'
+#        show_common_ansible_types()
+
+#        show_refs(filename='task_include_refs', objs=tis, max_depth=6, max_objs=1)
+        # show_refs(filename='task_include_refs', objs=funcs, max_depth=6, max_objs=1)
         self.prev_mem = track_mem(msg=msg, pid=pid, call_stack=call_stack, subsystem=subsystem,
                                   prev_mem=self.prev_mem)
         return self.prev_mem
 
     # FIXME: base Strategy.run has a result kwarg, but lineary does not
     def run(self, iterator, play_context, result=0):
-        self.track_mem(msg='before run')
+        # self.track_mem(msg='before run')
         res = super(StrategyModule, self).run(iterator, play_context)
-        self.track_mem(msg='after run')
+        # self.track_mem(msg='after run')
+        print('after strat.run')
+        self.show_backrefs()
 
-        show_common_ansible_types()
+        # self.show_backrefs()
+        #show_common_ansible_types()
 
+##        # example of dumping graphviz dot/pngs for ref graph of some objs
+        # funcs = objgraph.by_type('ansible.playbook.task_include.TaskInclude')
+        #funcs = objgraph.by_type('__builtin__.dict')
+        #funcs = objgraph.by_type('ansible.parsing.yaml.objects.AnsibleUnicode')
+
+#        show_refs(filename='task_include_refs', objs=tis, max_depth=6, max_objs=1)
+        #show_refs(filename='task_include_refs', objs=funcs, max_depth=6, max_objs=1)
 
         return res
-
-
-        # example of dumping graphviz dot/pngs for ref graph of some objs
-#        tis = objgraph.by_type('ansible.playbook.task_include.TaskInclude')
-        #import pprint
-        #for i in tis:
-        #    pprint.pprint(i.__dict__)
-#        show_refs(filename='task_include_refs', objs=tis, max_depth=6, max_objs=1)
-
-#        things = objgraph.by_type('ansible.playbook.attribute.FieldAttribute')
-#        show_refs(filename='things_refs', objs=things, max_depth=6, max_objs=2)
-
-#        things2 = objgraph.by_type('ansible.playbook.block.Block')
-#        show_refs(filename='things2_refs', objs=things2, max_depth=6, max_objs=2)
-
-#        return res
 
     def add_tqm_variables(self, vars, play):
         self.track_mem(msg='before add_tqm_variables')
@@ -211,6 +285,7 @@ class StrategyModule(LinearStrategyModule):
 
     def _process_pending_results(self, iterator, one_pass=False, max_passes=None):
         self.track_mem(msg='before _process_pending_results')
+        self.show_backrefs()
         res = super(StrategyModule, self)._process_pending_results(iterator, one_pass, max_passes)
         self.track_mem(msg='after _process_pending_results')
         return res
