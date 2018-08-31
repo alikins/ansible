e6b32c4640447364385382db34d018b4acc8bbb5
Adrian Likins <alikins@redhat.com>
2018-08-31 18:14:25 -0400

    Update to use AnsibleModule arg spec handling
    Now returns a list of all argument errors not
    just the first one (in the results['argument_errors']
    value).

--- lib/ansible/plugins/action/validate_survey_spec.py
+++ lib/ansible/plugins/action/validate_survey_spec.py
@@ -7,18 +7,69 @@ __metaclass__ = type
 import logging
 import pprint
 
-from ansible.errors import AnsibleError
+from ansible.errors import AnsibleError, AnsibleModuleError
 from ansible.plugins.action import ActionBase
-from ansible.playbook.role import survey
+from ansible.module_utils import basic
+from ansible.module_utils.six import iteritems, string_types
 
 log = logging.getLogger(__name__)
 pf = pprint.pformat
+
+
+class AnsibleArgSpecError(AnsibleModuleError):
+    def __init__(self, *args, **kwargs):
+        self.argument_errors = kwargs.pop('argument_errors', [])
+        super(AnsibleArgSpecError, self).__init__(*args, **kwargs)
+
+
+class ArgSpecValidatingAnsibleModule(basic.AnsibleModule):
+    '''AnsibleModule but with overridden _load_params so it doesnt read from stdin/ANSIBLE_ARGS'''
+    def __init__(self, *args, **kwargs):
+        self.params = kwargs.pop('params', {})
+        self.arg_validation_errors = []
+        super(ArgSpecValidatingAnsibleModule, self).__init__(*args, **kwargs)
+
+    def _load_params(self):
+        pass
+
+    def fail_json(self, *args, **kwargs):
+        msg = kwargs.pop('msg', 'Unknown arg spec validation error')
+        log.debug('Arg spec validation caused fail_json() to be called')
+        log.error('Arg spec validation error: %s', msg)
+        # log.debug('fail_json args: %s', repr(args))
+        # log.debug('fail_json kwargs: %s', repr(kwargs))
+        self.arg_validation_errors.append(msg)
+
+    def check_for_errors(self):
+        if self.arg_validation_errors:
+            raise AnsibleArgSpecError(argument_errors=self.arg_validation_errors)
 
 
 class ActionModule(ActionBase):
     ''' Validate a Role survey spec and answers to it's questions '''
 
     TRANSFERS_FILES = False
+
+    def build_args(self, argument_spec, task_vars):
+        args = {}
+        for key, attrs in iteritems(argument_spec):
+            if attrs is None:
+                argument_spec[key] = {'type': 'str'}
+            if key in task_vars:
+                if isinstance(task_vars[key], string_types):
+                    value = self._templar.do_template(task_vars[key])
+                    if value:
+                        args[key] = value
+                else:
+                    args[key] = task_vars[key]
+            elif attrs:
+                if 'aliases' in attrs:
+                    for item in attrs['aliases']:
+                        if item in task_vars:
+                            args[key] = self._templar.do_template(task_vars[key])
+                elif 'default' in attrs and key not in args:
+                    args[key] = attrs['default']
+        return args
 
     def run(self, tmp=None, task_vars=None):
         '''
@@ -39,55 +90,66 @@ class ActionModule(ActionBase):
         if 'argument_spec' not in self._task.args:
             raise AnsibleError('"argument_spec" arg is required in args')
 
-        argument_spec = self._task.args.get('argument_spec')
+        log.debug('self._task.args: %s', pf(self._task.args))
+
+        # get the task var called argument_spec
+        argument_spec_data = self._task.args.get('argument_spec')
+        log.debug('argument_spec_data: %s', argument_spec_data)
+
+        # then get the 'argument_spec' item from the dict in the argument_spec task var
+        argument_spec = argument_spec_data.get('argument_spec', [])
         log.debug('argument_spec: %s', argument_spec)
 
-        argument_spec_elements = argument_spec.get('spec', [])
-        log.debug('argument_spec_elements: %s', argument_spec_elements)
-
-        log.debug('self._task.args: %s', pf(self._task.args))
-        survey_answers = self._task.args.get('survey_answers', {})
         provided_arguments = self._task.args.get('provided_arguments', {})
 
         log.debug('provided_arguments: %s', provided_arguments)
-        log.debug('survey_answers: %s', survey_answers)
 
-        if not isinstance(argument_spec, dict):
-            raise AnsibleError('Incorrect type for argument_spec, expected dict and got %s' % type(argument_spec))
+        if not isinstance(argument_spec_data, dict):
+            raise AnsibleError('Incorrect type for argument_spec, expected dict and got %s' % type(argument_spec_data))
 
         if not isinstance(provided_arguments, dict):
             raise AnsibleError('Incorrect type for survey_data, expected dict and got %s' % type(provided_arguments))
 
-        argument_errors = []
-        for argument_spec_item in argument_spec_elements:
-            res = survey._survey_element_validation(argument_spec_item,
-                                                    provided_arguments,
-                                                    validate_required=True)
-            argument_errors.extend(res)
+        module_params = provided_arguments
+
+        log.debug('argument_spec_data: %s', pf(argument_spec_data))
+        log.debug('argument_spec: %s', pf(argument_spec))
+
+        built_args = self.build_args(argument_spec, task_vars)
+
+        log.debug('built_args: %s', pf(built_args))
+
+        module_args = {}
+        module_args['argument_spec'] = argument_spec
+        module_args['params'] = module_params
+
+        log.debug('module_args: %s', pf(module_args))
+
+        try:
+            validating_module = ArgSpecValidatingAnsibleModule(**module_args)
+            log.debug('validating_module: %s', validating_module)
+            validating_module.check_for_errors()
+        except AnsibleArgSpecError as e:
+            log.exception(e)
+            # log.exception(sys.exc_info)
+            log.error('role arg spec validation failed')
+            for error in e.argument_errors:
+                log.error('role arg validation error: %s', error)
+
+            # log.exception(e)
+            result['_ansible_verbose_always'] = True
+            result['failed'] = True
+            result['msg'] = e.message
+
+            # does this need to check no_log?
+            result['argument_errors'] = e.argument_errors
+
+            return result
 
         result['_ansible_verbose_always'] = True
-        if argument_errors:
-            result['failed'] = True
-            result['argument_errors'] = argument_errors
-
-            result['msg'] = 'There were validation errors in the survey'
-            return result
 
         result['changed'] = False
         result['msg'] = 'The survey validation passed'
-        result['valid_supplied_arguments'] = provided_arguments
+        result['valid_provided_arguments'] = provided_arguments
+
         return result
-
-    def validate_args(self, spec_elements, answers):
-        '''Validate the role arg spec and raise AnsibleModuleError if it fails
-
-        Return: void?
-        '''
-        # build arg spec
-        # dump it as ansiballz/AnsibleModuleish arg format
-        # monkeypath module_utils.basic _ANSIBLE_ARGS, AnsibleModule.fail_json (???)
-        # build faux-ish AnsibleModule
-        # monkeypath AnsibleModule
-        #  call AnsibleModule(**spec) to do it's arg validation and fail_json
-        # raise from fail_json if called
-        pass
