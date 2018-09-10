24fa5b1106edf09cba5fe465330094a3b7cf4be1
Adrian Likins <alikins@redhat.com>
2018-09-10 13:34:50 -0400

    More intg tests.
    Skipping test_import_role.yml because it is doing something
    odd.

--- test/integration/targets/validate_arg_spec/runme.sh
+++ test/integration/targets/validate_arg_spec/runme.sh
@@ -2,4 +2,17 @@
 
 set -euvx
 
-ansible-playbook -i ../../inventory -v "$@" test_include_role_fails.yml
+# if you need to point to a custom ansible-playbook, perhaps
+# one with logging enabled
+ANSIBLE_PLAYBOOK=${ANSIBLE_PLAYBOOK:-"ansible-playbook"}
+
+# test with some bogus argspecs that shoudl cause validate_arg_spec to fail
+"$ANSIBLE_PLAYBOOK" -i ../../inventory -v "$@" test_include_role_fails.yml
+
+# test with valid arg specs
+"$ANSIBLE_PLAYBOOK" -i ../../inventory -v "$@" test_include_role.yml
+
+# not sure what is going on here, multiple import_role tasks seem to clobber each others vars:
+# "$ANSIBLE_PLAYBOOK" -i ../../inventory -v "$@" -e @../../integration_config.yml test_import_role.yml
+
+"$ANSIBLE_PLAYBOOK" -i ../../inventory -v "$@" -e @../../integration_config.yml test_play_level_role.yml
