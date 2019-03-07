0bb2d961bcf465693507e5c81a87926bf5ad1490
Adrian Likins <alikins@redhat.com>
2019-03-07 13:16:07 -0500

    task_executor was trying to find all action plugins
    Previously, if task_executor.TaskExecutor._get_action_handle tried
    to find an action plugin that has not been already loaded (or doesnt
    exist),  it would try to find a action plugin with a name based on
    a 'module_prefix' created by splitting the requested name on '_'.
    For collections, this results in it looking for something
    like 'somenamespace.some' if trying to find a
    'somenamespace.some_name.some_action_name'.
    
    This is incorrect except for the special case of some networking
    action plugins, but the all() always runs all the expressions in
    it, so a 'module_prefix in self._shared_loader_obj.action_loader'.
    
    The 'module_prefix in self._shared_loader_obj.action_loader' results
    on the action_loader running it's find_plugins, which potentially
    causes the loader to raise inport/loader exceptions since a
    module_prefix like 'somenamespace.some' is bogus and doesnt exist
    and can't be loaded.
    
    Replacing the all() with an 'and' expression that will short circuit
    if not the network group module case fixes it. This prevents the
    loader from trying to find a 'module_prefix' that will not exist.

--- lib/ansible/executor/task_executor.py
+++ lib/ansible/executor/task_executor.py
@@ -1015,7 +1015,7 @@ class TaskExecutor:
         # let action plugin override module, fallback to 'normal' action plugin otherwise
         if self._shared_loader_obj.action_loader.has_plugin(self._task.action, collection_list=self._task.collections):
             handler_name = self._task.action
-        elif all((module_prefix in C.NETWORK_GROUP_MODULES, module_prefix in self._shared_loader_obj.action_loader)):
+        elif module_prefix in C.NETWORK_GROUP_MODULES and module_prefix in self._shared_loader_obj.action_loader:
             handler_name = module_prefix
         else:
             # FIXME: once we have ansible.core, preface this action with it so we don't allow it to be hijacked
