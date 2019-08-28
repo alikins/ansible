48d3fe435db8bf43e0f940b0efaa2587c55561bd
Adrian Likins <alikins@redhat.com>
2019-08-28 12:09:27 -0400

    Revert "Set GalaxyAPI inst 'token_type' if we know it's api version"
    This reverts commit 8c20a5ce3fd5557236d08f6aef98d7cf9669d7c7.

--- lib/ansible/galaxy/api.py
+++ lib/ansible/galaxy/api.py
@@ -66,13 +66,12 @@ class GalaxyAPI(object):
 
     SUPPORTED_VERSIONS = ['v1']
 
-    def __init__(self, galaxy, name, url, username=None, password=None, token=None, token_type=None):
+    def __init__(self, galaxy, name, url, username=None, password=None, token=None):
         self.galaxy = galaxy
         self.name = name
         self.username = username
         self.password = password
         self.token = token
-        self.token_type = token_type or 'Token'
         self.api_server = url
         self.validate_certs = not context.CLIARGS['ignore_certs']
         self.baseurl = None
@@ -82,14 +81,12 @@ class GalaxyAPI(object):
 
         display.debug('Validate TLS certificates for %s: %s' % (self.api_server, self.validate_certs))
 
-    def _auth_header(self, required=True, token_type=None):
+    def _auth_header(self, required=True, token_type='Token'):
         '''Generate the Authorization header.
 
         Valid token_type values are 'Token' (galaxy v2) and 'Bearer' (galaxy v3)'''
         token = self.token.get() if self.token else None
 
-        # 'Token' for v2 api, 'Bearer' for v3
-        token_type = token_type or self.token_type
         if token:
             return {'Authorization': "%s %s" % (token_type, token)}
         elif self.username:
